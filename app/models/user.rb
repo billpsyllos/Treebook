011b5968d0170d4742a2bf2bca00d5f6c288b447
billpsyllos <billpsyllos1@gmail.com>
2014-05-12 14:47:47 +0300

    model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  
+  def full_name
+  	full_name + " " + last_name
+  end
 end
